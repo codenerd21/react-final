e30a38611ace5dfe08c97d35a427a7ee14fc46ae
Braxton Toomer <braxton.toomer@gmail.com>
2019-10-08 11:26:05 -0700

    verify jwts

---
+++ routes/api/scores.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const scoresCtrl = require('../../controllers/scores');
+
+router.get('/', scoresCtrl.highScores);
+
+/*---------- Protected Routes ----------*/
+// Process the token for only the routes below
+router.use(require('../../config/auth'));
+router.post('/', scoresCtrl.create);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api/users', require('./routes/api/users'));
 
+app.use(require('./config/auth'));
+app.use('/api/scores', require('./routes/api/scores'));
+
 // "Catch All" Route
 
 app.get('/*', function (req, res) {
